6b8c5b591e4d7195a031fcd39a89a2e7e5960a66
John Knotts <jhnnyk@gmail.com>
2024-06-18 12:02:49 -0600

    create recipe form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,57 @@
+import { useState } from 'react'
+
+// styles
 import './Create.css'
 
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime);
+  }
+
   return (
-    <div>Create page</div>
+    <div className='create'>
+      <h2 className="page-title">Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input 
+            type="text" 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title} // sets up the two-way binding in case the value
+                          // is changed from somewhere else in the program
+            required
+          />
+        </label>
+
+        {/* ingredients go here */}
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea 
+            onChange={(e) => setMethod(e.target.value)} 
+            value={method} 
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input 
+            type="number" 
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className='btn'>submit</button>
+      </form>
+    </div>
   )
 }
